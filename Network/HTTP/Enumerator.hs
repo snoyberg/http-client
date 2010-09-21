72e8130e3624fb3284515a1523acd7d76521e7b1
Michael Snoyman <michael@snoyman.com>
2010-09-21 22:19:27 +0200

    Introduced simplified API

--- Network/HTTP/Enumerator.hs
+++ Network/HTTP/Enumerator.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Network.HTTP.Enumerator
     ( Request (..)
     , Response (..)
     , http
+    , parseUrl
+    , httpLbs
+    , simpleHttp
     ) where
 
 import qualified OpenSSL.Session as SSL
@@ -12,15 +17,20 @@ import qualified Network.Socket.ByteString as B
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as S8
-import Data.Enumerator hiding (head, map)
+import Data.Enumerator hiding (head, map, break)
 import qualified Data.Enumerator as E
 import Http
 import Safe
-import Control.Exception (throwIO)
+import Control.Exception (throwIO, Exception)
 import Control.Arrow (first)
 import Data.Char (toLower)
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
+import Control.Failure
+import Data.Typeable (Typeable)
+import Data.Word (Word8)
+import Data.Bits
+import Data.Maybe (fromMaybe)
 
 getSocket :: String -> Int -> IO Socket
 getSocket host' port' = do
@@ -83,7 +93,6 @@ data Request = Request
 
 data Response a = Response
     { statusCode :: Int
-    , statusMessage :: S.ByteString
     , responseHeaders :: [(S.ByteString, S.ByteString)]
     , responseBody :: a
     }
@@ -119,7 +128,7 @@ http (Request {..}) bodyIter = do
         hcWrite hc "\r\n"
         mapM_ (hcWrite hc) $ L.toChunks requestBody
         run $ connToEnum hc $$ do
-            ((_, sc, sm), hs) <- iterHeaders
+            ((_, sc, _), hs) <- iterHeaders
             let hs' = map (first $ S8.map toLower) hs
             let mcl = lookup "content-length" hs'
             body' <-
@@ -134,7 +143,6 @@ http (Request {..}) bodyIter = do
                 Right body'' ->
                     return $ Response
                         { statusCode = sc
-                        , statusMessage = sm
                         , responseHeaders = hs
                         , responseBody = body''
                         }
@@ -180,3 +188,97 @@ encodeUrlChar y =
             | x < 16 = toEnum $ x - 10 + (fromEnum 'A')
             | otherwise = error $ "Invalid argument to showHex: " ++ show x
      in ['%', showHex' b, showHex' c]
+
+data InvalidUrlException = InvalidUrlException String String
+    deriving (Show, Typeable)
+instance Exception InvalidUrlException
+
+parseUrl :: Failure InvalidUrlException m => String -> m Request
+parseUrl s@('h':'t':'t':'p':':':'/':'/':rest) = parseUrl1 s False rest
+parseUrl s@('h':'t':'t':'p':'s':':':'/':'/':rest) = parseUrl1 s True rest
+parseUrl x = failure $ InvalidUrlException x "Invalid scheme"
+
+parseUrl1 :: Failure InvalidUrlException m
+          => String -> Bool -> String -> m Request
+parseUrl1 full sec s = do
+    port' <- mport
+    return Request
+        { host = S8.pack hostname -- FIXME check chars
+        , port = port'
+        , secure = sec
+        , requestHeaders = []
+        , path = S8.pack $ if null path' then "/" else path' -- FIXME check chars
+        , queryString = parseQueryString $ S8.pack qstring -- FIXME check chars
+        , requestBody = L.empty
+        , method = "GET"
+        }
+  where
+    (beforeSlash, afterSlash) = break (== '/') s
+    (hostname, portStr) = break (== ':') beforeSlash
+    (path', qstring') = break (== '?') afterSlash
+    qstring'' = case qstring' of
+                '?':x -> x
+                _ -> qstring'
+    qstring = takeWhile (/= '#') qstring''
+    mport =
+        case (portStr, sec) of
+            ("", False) -> return 80
+            ("", True) -> return 443
+            (':':rest, _) ->
+                case readMay rest of
+                    Just i -> return i
+                    Nothing -> failure $ InvalidUrlException full "Invalid port"
+            x -> error $ "parseUrl1: this should never happen: " ++ show x
+
+parseQueryString :: S.ByteString -> [(S.ByteString, S.ByteString)]
+parseQueryString = parseQueryString' . dropQuestion
+  where
+    dropQuestion q | S.null q || S.head q /= 63 = q
+    dropQuestion q | otherwise = S.tail q
+    parseQueryString' q | S.null q = []
+    parseQueryString' q =
+        let (x, xs) = breakDiscard 38 q -- ampersand
+         in parsePair x : parseQueryString' xs
+      where
+        parsePair x =
+            let (k, v) = breakDiscard 61 x -- equal sign
+             in (qsDecode k, qsDecode v)
+
+
+qsDecode :: S.ByteString -> S.ByteString
+qsDecode z = fst $ S.unfoldrN (S.length z) go z
+  where
+    go bs =
+        case uncons bs of
+            Nothing -> Nothing
+            Just (43, ws) -> Just (32, ws) -- plus to space
+            Just (37, ws) -> Just $ fromMaybe (37, ws) $ do -- percent
+                (x, xs) <- uncons ws
+                x' <- hexVal x
+                (y, ys) <- uncons xs
+                y' <- hexVal y
+                Just $ (combine x' y', ys)
+            Just (w, ws) -> Just (w, ws)
+    hexVal w
+        | 48 <= w && w <= 57  = Just $ w - 48 -- 0 - 9
+        | 65 <= w && w <= 70  = Just $ w - 55 -- A - F
+        | 97 <= w && w <= 102 = Just $ w - 87 -- a - f
+        | otherwise = Nothing
+    combine :: Word8 -> Word8 -> Word8
+    combine a b = shiftL a 4 .|. b
+
+uncons :: S.ByteString -> Maybe (Word8, S.ByteString)
+uncons s
+    | S.null s = Nothing
+    | otherwise = Just (S.head s, S.tail s)
+
+breakDiscard :: Word8 -> S.ByteString -> (S.ByteString, S.ByteString)
+breakDiscard w s =
+    let (x, y) = S.break (== w) s
+     in (x, S.drop 1 y)
+
+httpLbs :: Request -> IO (Response L.ByteString)
+httpLbs = flip http (L.fromChunks `fmap` consume)
+
+simpleHttp :: String -> IO (Response L.ByteString)
+simpleHttp url = parseUrl url >>= httpLbs
--- test.hs
+++ test.hs
@@ -6,20 +6,24 @@ import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Enumerator (consume, Iteratee)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = withSocketsDo $ withOpenSSL $ do
-    Response sc sm hs b <- http Request
-        { host = "localhost"
-        , port = 80
-        , secure = False
-        , requestHeaders = []
-        , path = "/"
-        , queryString = [("foo", "bar")]
-        , requestBody = L8.pack "baz=bin"
-        , method = "POST"
-        } toLBS
-    print (sc, sm)
+    let _req1 = Request
+            { host = "localhost"
+            , port = 80
+            , secure = False
+            , requestHeaders = []
+            , path = "/"
+            , queryString = [("foo", "bar")]
+            , requestBody = L8.pack "baz=bin"
+            , method = "POST"
+            }
+    [url] <- getArgs
+    _req2 <- parseUrl url
+    Response sc hs b <- http _req2 toLBS
+    print sc
     mapM_ (\(x, y) -> do
         S.putStr x
         putStr ": "
