3702550b3484fc08236d0907493e524e5ec2e945
Michael Snoyman <michael@snoyman.com>
2012-03-23 15:38:02 +0200

    not overlong headers test case

--- test/main.hs
+++ test/main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 import Test.Hspec.Monadic
+import qualified Data.ByteString as S
 import Test.Hspec.HUnit ()
 import Test.HUnit
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Conduit
-import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent (forkIO, killThread, threadDelay)
 import Network.HTTP.Types
 import Control.Exception.Lifted (try, SomeException)
 import Network.HTTP.Conduit.ConnInfo
@@ -16,9 +17,11 @@ import Data.Conduit (($$))
 import Control.Monad.Trans.Resource (register)
 import Control.Monad.IO.Class (liftIO)
 import Data.ByteString.UTF8 (fromString)
-import Data.Conduit.List (sourceList)
+import Data.Conduit.List (sourceList, sinkNull)
 import Data.CaseInsensitive (mk)
 import Data.List (partition)
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Binary as CB
 
 app :: Application
 app req =
@@ -60,6 +63,7 @@ main = hspecX $ do
             clearSocketsList
             tid1 <- forkIO $ run 3002 app
             tid2 <- forkIO $ run 3003 app
+            threadDelay 1000
             withManager $ \manager -> do
                 let Just req1 = parseUrl "http://localhost:3002/"
                 let Just req2 = parseUrl "http://localhost:3003/"
@@ -73,6 +77,7 @@ main = hspecX $ do
     describe "DOS protection" $ do
         it "overlong headers" $ do
             tid1 <- forkIO overLongHeaders
+            threadDelay 1000
             withManager $ \manager -> do
                 _ <- register $ killThread tid1
                 let Just req1 = parseUrl "http://localhost:3004/"
@@ -80,9 +85,24 @@ main = hspecX $ do
                 case res1 of
                     Left e -> liftIO $ show (e :: SomeException) @?= show OverlongHeaders
                     _ -> error "Shouldn't have worked"
+        it "not overlong headers" $ do
+            tid1 <- forkIO notOverLongHeaders
+            threadDelay 1000
+            withManager $ \manager -> do
+                _ <- register $ killThread tid1
+                let Just req1 = parseUrl "http://localhost:3005/"
+                _ <- httpLbs req1 manager
+                return ()
 
 overLongHeaders :: IO ()
 overLongHeaders = runTCPServer (ServerSettings 3004 HostAny) $ \_ sink ->
     src $$ sink
   where
     src = sourceList $ "HTTP/1.0 200 OK\r\nfoo: " : repeat "bar"
+
+notOverLongHeaders :: IO ()
+notOverLongHeaders = runTCPServer (ServerSettings 3005 HostAny) $ \src' sink -> do
+    src' $$ CL.drop 1
+    src $$ sink
+  where
+    src = sourceList $ [S.concat $ "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 16384\r\n\r\n" : ( take 16384 $ repeat "x")]
