e35228540fbd77b17f99740b3450576dbcb36cf4
Michael Snoyman <michael@snoyman.com>
2015-04-28 14:52:42 +0300

    Drop utf8-string dependency

--- Network/PublicSuffixList/Serialize.hs
+++ Network/PublicSuffixList/Serialize.hs
@@ -1,11 +1,12 @@
 module Network.PublicSuffixList.Serialize (getDataStructure, putDataStructure) where
 
-import qualified Data.ByteString      as BS
-import qualified Data.ByteString.UTF8 as U8
+import qualified Data.ByteString          as BS
 import           Data.Functor
-import           Data.Serialize.Get hiding (getTreeOf)
+import           Data.Serialize.Get       hiding (getTreeOf)
 import           Data.Serialize.Put
-import qualified Data.Text            as T
+import qualified Data.Text                as T
+import qualified Data.Text.Encoding       as TE
+import qualified Data.Text.Encoding.Error as TEE
 
 import Network.PublicSuffixList.Types
 
@@ -13,7 +14,7 @@ getTreeOf :: Ord k => Get k -> Get (Tree k)
 getTreeOf p = Node <$> getMapOf p (getTreeOf p)
 
 getText :: Get T.Text
-getText = (T.pack . U8.toString . BS.pack) <$> getListOf getWord8
+getText = (TE.decodeUtf8With TEE.lenientDecode . BS.pack) <$> getListOf getWord8
 
 getDataStructure :: Get DataStructure
 getDataStructure = getTwoOf (getTreeOf getText) (getTreeOf getText)
@@ -22,7 +23,7 @@ putTree :: Ord k => Putter k -> Putter (Tree k)
 putTree p = putMapOf p (putTree p) . children
 
 putText :: Putter T.Text
-putText = putListOf putWord8 . BS.unpack . U8.fromString . T.unpack
+putText = putListOf putWord8 . BS.unpack . TE.encodeUtf8
 
 putDataStructure :: Putter DataStructure
 putDataStructure = putTwoOf (putTree putText) (putTree putText)
