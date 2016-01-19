50b56deb255245c68325d126b455472183e1d31e
N. K. Michalopulos <niklas.michalopulos@rise-world.com>
2016-01-19 14:32:17 +0100

    Enable managerModifyRequest to modify checkStatus

--- http-client/test/Network/HTTP/ClientSpec.hs
+++ http-client/test/Network/HTTP/ClientSpec.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.ClientSpec where
 
+import           Control.Exception         (toException)
 import           Network                   (withSocketsDo)
 import           Network.HTTP.Client
 import           Network.HTTP.Types        (status200)
@@ -24,3 +25,9 @@ spec = describe "Client" $ do
         withManager settings $ \man -> do
             res <- httpLbs "http://httpbin.org:1234" man
             responseStatus res `shouldBe` status200
+
+    it "managerModifyRequestCheckStatus" $ do
+        let modify req = return req { checkStatus = \s hs cj -> Just $ toException $ StatusCodeException s hs cj }
+            settings = defaultManagerSettings { managerModifyRequest = modify }
+        withManager settings $ \man ->
+            httpLbs "http://httpbin.org" man `shouldThrow` anyException
