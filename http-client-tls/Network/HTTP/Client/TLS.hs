91b3cc8e893395d35c7796dd8c703fc37eb5dfe8
Michael Snoyman <michael@snoyman.com>
2016-04-14 07:32:00 +0300

    Global manager

--- http-client-tls/Network/HTTP/Client/TLS.hs
+++ http-client-tls/Network/HTTP/Client/TLS.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 -- | Support for making connections via the connection package and, in turn,
 -- the tls package suite.
+--
+-- Recommended reading: <https://github.com/commercialhaskell/jump/blob/master/doc/http-client.md>
 module Network.HTTP.Client.TLS
-    ( tlsManagerSettings
+    ( -- * Settings
+      tlsManagerSettings
     , mkManagerSettings
+      -- * Global manager
+    , getGlobalManager
+    , setGlobalManager
+      -- * Internal
     , getTlsConnection
     ) where
 
@@ -15,7 +22,11 @@ import qualified Network.Connection as NC
 import Network.Socket (HostAddress)
 import qualified Network.TLS as TLS
 import qualified Data.ByteString as S
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import System.IO.Unsafe (unsafePerformIO)
 
+-- | Create a TLS-enabled 'ManagerSettings' with the given 'NC.TLSSettings' and
+-- 'NC.SockSettings'
 mkManagerSettings :: NC.TLSSettings
                   -> Maybe NC.SockSettings
                   -> ManagerSettings
@@ -54,6 +65,7 @@ mkManagerSettings tls sock = defaultManagerSettings
          in handle $ throwIO . wrapper
     }
 
+-- | Default TLS-enabled manager settings
 tlsManagerSettings :: ManagerSettings
 tlsManagerSettings = mkManagerSettings def Nothing
 
@@ -106,3 +118,20 @@ convertConnection conn = makeConnection
     -- on the socket.  But when this is called the socket might be
     -- already closed, and we get a @ResourceVanished@.
     (NC.connectionClose conn `Control.Exception.catch` \(_ :: IOException) -> return ())
+
+-- | Evil global manager, to make life easier for the common use case
+globalManager :: IORef Manager
+globalManager = unsafePerformIO (newManager tlsManagerSettings >>= newIORef)
+{-# NOINLINE globalManager #-}
+
+-- | Get the current global 'Manager'
+--
+-- @since 0.2.4
+getGlobalManager :: IO Manager
+getGlobalManager = readIORef globalManager
+
+-- | Set the current global 'Manager'
+--
+-- @since 0.2.4
+setGlobalManager :: Manager -> IO ()
+setGlobalManager = writeIORef globalManager
--- http-client-tls/README.md
+++ http-client-tls/README.md
@@ -1,5 +1,8 @@
 ## http-client-tls
 
+Full tutorial docs are available at:
+https://github.com/commercialhaskell/jump/blob/master/doc/http-client.md
+
 Use the http-client package with the pure-Haskell tls package for secure
 connections. For the most part, you'll just want to replace
 `defaultManagerSettings` with `tlsManagerSettings`, e.g.:
