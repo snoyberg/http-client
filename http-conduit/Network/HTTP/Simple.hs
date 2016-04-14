da6a41429171ce66ff5167d5bb13f77e011fa9c2
Michael Snoyman <michael@snoyman.com>
2016-04-14 10:18:38 +0300

    Initial lens-based accessors and setters for requests

--- http-conduit/Network/HTTP/Simple.hs
+++ http-conduit/Network/HTTP/Simple.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RankNTypes         #-}
+{-# LANGUAGE TupleSections      #-}
 -- | Simplified interface for common HTTP client interactions. Tutorial
 -- available at
 -- <https://github.com/commercialhaskell/jump/blob/master/doc/http-client.md>.
@@ -25,6 +27,25 @@ module Network.HTTP.Simple
     , H.Response
     , JSONException (..)
     , H.HttpException (..)
+      -- * Request constructions
+    , defaultRequest
+    , parseRequest
+      -- * Request lenses
+      -- ** Basics
+    , requestMethod
+    , requestSecure
+    , requestHost
+    , requestPort
+    , requestPath
+    , requestHeader
+    , requestHeaders
+    , requestQueryString
+      -- ** Request body
+      -- ** Special fields
+    , requestIgnoreStatus
+    , requestBasicAuth
+    , requestManager
+      -- * Response lenses
       -- * Alternate spellings
     , httpLbs
     ) where
@@ -32,6 +53,7 @@ module Network.HTTP.Simple
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import qualified Network.HTTP.Client as H
+import qualified Network.HTTP.Client.Internal as HI
 import qualified Network.HTTP.Client.TLS as H
 import Network.HTTP.Client.Conduit (bodyReaderSource)
 import Control.Monad.IO.Class (MonadIO, liftIO)
@@ -43,7 +65,9 @@ import Control.Exception (throwIO, Exception)
 import Data.Typeable (Typeable)
 import qualified Data.Conduit as C
 import qualified Data.Conduit.Attoparsec as C
-import Control.Monad.Catch (MonadMask, bracket)
+import qualified Control.Monad.Catch as Catch
+import Data.Default.Class (def)
+import qualified Network.HTTP.Types as H
 
 -- | Perform an HTTP request and return the body as a lazy @ByteString@. Note
 -- that the entire value will be read into memory at once (no lazy I\/O will be
@@ -91,16 +115,40 @@ data JSONException
   deriving (Show, Typeable)
 instance Exception JSONException
 
+-- | The default request value. You'll almost certainly want to set the
+-- 'requestHost', and likely the 'requestPath' as well.
+--
+-- See also 'parseRequest'
+--
+-- @since 0.2.4
+defaultRequest :: H.Request
+defaultRequest = def
+
+-- | Parse a 'H.Request' from a 'String'. This is given as a URL, with an
+-- optional leading request method, e.g.:
+--
+-- * @http://example.com@
+-- * @https://example.com:1234/foo/bar?baz=bin@
+-- * @PUT http://example.com/some-resource@
+--
+-- If parsing fails, 'Catch.throwM' will be called. The behavior of this
+-- function is also used for the @IsString@ instance for use with
+-- @OverloadedStrings@.
+--
+-- @since 0.2.4
+parseRequest :: Catch.MonadThrow m => String -> m H.Request
+parseRequest = H.parseUrl
+
 -- | Perform an HTTP request and consume the body with the given 'C.Sink'
 --
 -- @since 0.2.4
-httpSink :: (MonadIO m, MonadMask m)
+httpSink :: (MonadIO m, Catch.MonadMask m)
          => H.Request
          -> (H.Response () -> C.Sink S.ByteString m a)
          -> m (H.Response a)
 httpSink req sink = do
     man <- liftIO H.getGlobalManager
-    bracket
+    Catch.bracket
         (liftIO $ H.responseOpen req man)
         (liftIO . H.responseClose)
         (\res -> T.mapM ((C.$$ sink (plain res)) . bodyReaderSource) res)
@@ -113,22 +161,113 @@ httpSink req sink = do
 httpLbs :: MonadIO m => H.Request -> m (H.Response L.ByteString)
 httpLbs = httpLBS
 
+type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+type Lens' s a = Lens s s a a
+
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens get set f s = fmap (set s) (f (get s))
+
+-- | Lens for the request method
+--
+-- @since 0.2.4
+requestMethod :: Lens' H.Request S.ByteString
+requestMethod = lens H.method (\r x -> r { H.method = x })
+
+-- | Lens for whether this is a secure/HTTPS (@True@) or insecure/HTTP
+-- (@False@) request
+--
+-- @since 0.2.4
+requestSecure :: Lens' H.Request Bool
+requestSecure = lens H.secure (\r x -> r { H.secure = x })
+
+-- | Lens for the destination host of the request
+--
+-- @since 0.2.4
+requestHost :: Lens' H.Request S.ByteString
+requestHost = lens H.host (\r x -> r { H.host = x })
+
+-- | Lens for the destination port of the request
+--
+-- @since 0.2.4
+requestPort :: Lens' H.Request Int
+requestPort = lens H.port (\r x -> r { H.port = x })
+
+-- | Lens for the requested path info of the request
+--
+-- @since 0.2.4
+requestPath :: Lens' H.Request S.ByteString
+requestPath = lens H.path (\r x -> r { H.path = x })
+
+-- | Lens into all request header values with the given name
+--
+-- @since 0.2.4
+requestHeader :: H.HeaderName -> Lens' H.Request [S.ByteString]
+requestHeader name = lens
+    (map snd . filter (\(x, _) -> x == name) . H.requestHeaders)
+    (\req vals -> req
+        { H.requestHeaders =
+            filter (\(x, _) -> x /= name) (H.requestHeaders req)
+         ++ (map (name, ) vals)
+        })
+
+-- | Lens into all request headers
+--
+-- @since 0.2.4
+requestHeaders :: Lens' H.Request [(H.HeaderName, S.ByteString)]
+requestHeaders = lens H.requestHeaders (\req x -> req { H.requestHeaders = x })
+
+-- | Lens into the query string
+--
+-- @since 0.2.4
+requestQueryString :: Lens' H.Request [(S.ByteString, Maybe S.ByteString)]
+requestQueryString = lens
+    (H.parseQuery . H.queryString)
+    (flip H.setQueryString)
+
+-- | Modify the request so that non-2XX status codes do not generate a runtime
+-- exception. If @True@, ignore the status code. If @False@, do the default 2XX
+-- check.
+--
+-- Note that you cannot inspect the current ignore status.
+--
+-- @since 0.2.4
+requestIgnoreStatus :: Lens H.Request H.Request () Bool
+requestIgnoreStatus = lens (const ()) (\req b ->
+    req { H.checkStatus =
+            if b
+                then \_ _ _ -> Nothing
+                else H.checkStatus def
+        })
+
+-- | Set basic auth with the given username and password
+--
+-- Note that you cannot inspect the username and password after setting
+--
+-- @since 0.2.4
+requestBasicAuth :: Lens H.Request H.Request () (S.ByteString, S.ByteString)
+requestBasicAuth = lens
+    (const ())
+    (\req (user, pass) -> H.applyBasicAuth user pass req)
+
+-- | Instead of using the default global 'H.Manager', use the supplied
+-- @Manager@.
+--
+-- @since 0.2.4
+requestManager :: Lens' H.Request (Maybe H.Manager)
+requestManager = lens
+    HI.requestManagerOverride
+    (\req x -> req { HI.requestManagerOverride = x })
+
 -- TODO accessors and setters for various request and response fields. Will
 -- this be lens based? Still need to decide on that.
 --
 -- Minimal functionality:
 --
--- * Set request method
 -- * Set JSON request body
 -- * Set LBS request body
 -- * Set Source request body
 -- * Set file request body
--- * Add request headers
 -- * Set URL encoded body
--- * Disable exceptions on non-2XX
--- * applyBasicAuth
--- * Add to query string (or just set the whole thing?)
--- * Set request: secure, host, port, path
 --
 -- * Get response status
 -- * Get response status code (just the Int)
