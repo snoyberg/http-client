242f3511c6966c4cd1a02b0b4c333c0e24755931
amesgen <amesgen@amesgen.de>
2020-10-08 12:50:40 +0200

    provide reasonable default OpenSSL settings

--- http-client-openssl/Network/HTTP/Client/OpenSSL.hs
+++ http-client-openssl/Network/HTTP/Client/OpenSSL.hs
@@ -1,83 +1,51 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP #-}
 -- | Support for making connections via the OpenSSL library.
 module Network.HTTP.Client.OpenSSL
-    ( opensslManagerSettings
-    -- , defaultMakeContext
-    , withOpenSSL
+    ( withOpenSSL
+    , newOpenSSLManager
+    , opensslManagerSettings
+    , defaultMakeContext
     ) where
 
 import Network.HTTP.Client
 import Network.HTTP.Client.Internal
 import Control.Exception
+import Control.Monad.IO.Class
 import Network.Socket.ByteString (sendAll, recv)
 import OpenSSL
 import qualified Data.ByteString as S
 import qualified Network.Socket as N
-import qualified OpenSSL.Session       as SSL
+import qualified OpenSSL.Session as SSL
+import qualified OpenSSL.X509.SystemStore as SSL (contextLoadSystemCerts)
 
--- | Note that it is the caller's responsibility to pass in an appropriate
--- context. Future versions of http-client-openssl will hopefully include a
--- sane, safe default.
+-- | Create a new 'Manager' using 'opensslManagerSettings' and 'defaultMakeContext'.
+newOpenSSLManager :: MonadIO m => m Manager
+newOpenSSLManager = liftIO $ do
+  -- sharing an SSL context between threads (without modifying it) is safe:
+  -- https://github.com/openssl/openssl/issues/2165
+  ctx <- defaultMakeContext
+  newManager $ opensslManagerSettings (pure ctx)
+
+-- | Note that it is the caller's responsibility to pass in an appropriate context.
 opensslManagerSettings :: IO SSL.SSLContext -> ManagerSettings
 opensslManagerSettings mkContext = defaultManagerSettings
     { managerTlsConnection = do
         ctx <- mkContext
-        return $ \_ha host' port' -> do
-            -- Copied/modified from openssl-streams
-            let hints      = N.defaultHints
-                                { N.addrFlags      = [N.AI_ADDRCONFIG, N.AI_NUMERICSERV]
-                                , N.addrFamily     = N.AF_INET
-                                , N.addrSocketType = N.Stream
-                                }
-            (addrInfo:_) <- N.getAddrInfo (Just hints) (Just host') (Just $ show port')
-
-            let family     = N.addrFamily addrInfo
-            let socketType = N.addrSocketType addrInfo
-            let protocol   = N.addrProtocol addrInfo
-            let address    = N.addrAddress addrInfo
-
-            bracketOnError (N.socket family socketType protocol) (N.close)
-                $ \sock -> do
-                    N.connect sock address
-                    ssl <- SSL.connection ctx sock
-                    SSL.setTlsextHostName ssl host'
-                    SSL.connect ssl
-                    makeConnection
-                        (SSL.read ssl 32752 `catch` \(_ :: SSL.ConnectionAbruptlyTerminated) -> return S.empty)
-                        (SSL.write ssl)
-                        (N.close sock)
+        return $ \_ha host' port' ->
+            withSocket host' port' $ \sock ->
+                makeSSLConnection ctx sock host'
     , managerTlsProxyConnection = do
         ctx <- mkContext
-        return $ \connstr checkConn serverName _ha host' port' -> do
-            -- Copied/modified from openssl-streams
-            let hints      = N.defaultHints
-                                { N.addrFlags      = [N.AI_ADDRCONFIG, N.AI_NUMERICSERV]
-                                , N.addrFamily     = N.AF_INET
-                                , N.addrSocketType = N.Stream
-                                }
-            (addrInfo:_) <- N.getAddrInfo (Just hints) (Just host') (Just $ show port')
-
-            let family     = N.addrFamily addrInfo
-            let socketType = N.addrSocketType addrInfo
-            let protocol   = N.addrProtocol addrInfo
-            let address    = N.addrAddress addrInfo
-
-            bracketOnError (N.socket family socketType protocol) (N.close)
-                $ \sock -> do
-                    N.connect sock address
-                    conn <- makeConnection
-                            (recv sock 32752)
-                            (sendAll sock)
-                            (return ())
-                    connectionWrite conn connstr
-                    checkConn conn
-                    ssl <- SSL.connection ctx sock
-                    SSL.setTlsextHostName ssl serverName
-                    SSL.connect ssl
-                    makeConnection
-                        (SSL.read ssl 32752 `catch` \(_ :: SSL.ConnectionAbruptlyTerminated) -> return S.empty)
-                        (SSL.write ssl)
-                        (N.close sock)
+        return $ \connstr checkConn serverName _ha host' port' ->
+            withSocket host' port' $ \sock -> do
+                conn <- makeConnection
+                        (recv sock 32752)
+                        (sendAll sock)
+                        (return ())
+                connectionWrite conn connstr
+                checkConn conn
+                makeSSLConnection ctx sock serverName
 
     , managerRetryableException = \se ->
         case () of
@@ -98,3 +66,59 @@ opensslManagerSettings mkContext = defaultManagerSettings
         in
           handle (throwIO . wrap)
     }
+  where
+    withSocket host port use = do
+        -- Copied/modified from openssl-streams
+        let hints      = N.defaultHints
+                            { N.addrFlags      = [N.AI_ADDRCONFIG, N.AI_NUMERICSERV]
+                            , N.addrFamily     = N.AF_INET
+                            , N.addrSocketType = N.Stream
+                            }
+
+        (addrInfo:_) <- N.getAddrInfo (Just hints) (Just host) (Just $ show port)
+
+        let family     = N.addrFamily addrInfo
+        let socketType = N.addrSocketType addrInfo
+        let protocol   = N.addrProtocol addrInfo
+        let address    = N.addrAddress addrInfo
+
+        bracketOnError (N.socket family socketType protocol) (N.close) $
+            \sock -> N.connect sock address *> use sock
+
+    makeSSLConnection ctx sock host = do
+        ssl <- SSL.connection ctx sock
+        SSL.setTlsextHostName ssl host
+        SSL.enableHostnameValidation ssl host
+        SSL.connect ssl
+        makeConnection
+           (SSL.read ssl 32752 `catch` \(_ :: SSL.ConnectionAbruptlyTerminated) -> return S.empty)
+           (SSL.write ssl)
+           (N.close sock)
+
+-- | Make a new context that uses the system's trust anchors to verify
+-- server certificates. In particular:
+--
+--  * SSLv2 and SSLv3 are disabled
+--  * Server Name Indication
+--  * Hostname validation
+--  * @DEFAULT@ cipher list
+--
+-- Note that these settings might change in the future.
+defaultMakeContext :: IO SSL.SSLContext
+defaultMakeContext = do
+    ctx <- SSL.context
+    SSL.contextSetVerificationMode ctx
+        SSL.VerifyPeer
+          -- vpFailIfNoPeerCert and vpClientOnce are only relevant for servers
+          { SSL.vpFailIfNoPeerCert = False
+          , SSL.vpClientOnce = False
+          , SSL.vpCallback = Nothing
+          }
+    SSL.contextSetDefaultCiphers ctx
+    mapM_ (SSL.contextAddOption ctx)
+        [ SSL.SSL_OP_ALL -- enable bug workarounds
+        , SSL.SSL_OP_NO_SSLv2
+        , SSL.SSL_OP_NO_SSLv3
+        ]
+    SSL.contextLoadSystemCerts ctx
+    return ctx
