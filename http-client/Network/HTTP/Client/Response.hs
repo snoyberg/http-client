bd72ac4f54349aedcbf19d0d1b48fa3eaf60b367
meghfossa <megh@fossa.com>
2023-10-28 16:19:39 -0600

    Address PR feedback, and ensure entire chain of host gets apt headers

--- http-client/Network/HTTP/Client/Response.hs
+++ http-client/Network/HTTP/Client/Response.hs
@@ -14,6 +14,7 @@ import qualified Data.CaseInsensitive as CI
 import Control.Arrow (second)
 
 import Data.Monoid (mempty)
+import Data.List (nubBy)
 
 import qualified Network.HTTP.Types as W
 import Network.URI (parseURIReference, escapeURIString, isAllowedInURI)
@@ -50,7 +51,7 @@ import Data.KeyedPool
 -- >    applyCheckStatus (checkStatus req) res
 -- >    return redirectRequests
 getRedirectedRequest :: Request -> Request -> W.ResponseHeaders -> CookieJar -> Int -> Maybe Request
-getRedirectedRequest prevReq req hs cookie_jar code
+getRedirectedRequest origReq req hs cookie_jar code
     | 300 <= code && code < 400 = do
         l' <- lookup "location" hs
         let l = escapeURIString isAllowedInURI (S8.unpack l')
@@ -72,20 +73,32 @@ getRedirectedRequest prevReq req hs cookie_jar code
     cookie_jar' :: Maybe CookieJar
     cookie_jar' = fmap (const cookie_jar) $ cookieJar req
 
-    hostDiffer :: Bool
-    hostDiffer = host req /= host prevReq
+    hostDiffer :: Request -> Bool
+    hostDiffer req = host origReq /= host req
 
     shouldStripOnlyIfHostDiffer :: Bool
     shouldStripOnlyIfHostDiffer = shouldStripHeaderOnRedirectIfOnDifferentHostOnly req
 
+    mergeHeaders :: W.RequestHeaders -> W.RequestHeaders -> W.RequestHeaders
+    mergeHeaders lhs rhs = nubBy (\(a, _) (a', _) -> a == a') (lhs ++ rhs)
+    
     stripHeaders :: Request -> Request
-    stripHeaders r =
-        case (hostDiffer, shouldStripOnlyIfHostDiffer) of
+    stripHeaders r = do
+        case (hostDiffer r, shouldStripOnlyIfHostDiffer) of 
             (True, True) -> stripHeaders' r
             (True, False) -> stripHeaders' r
             (False, False) -> stripHeaders' r
-            (False, True) -> r
-    
+            (False, True) -> do
+                -- We need to check if we have omitted headers in previous
+                -- request chain. Consider request chain:
+                --
+                --  1. example-1.com 
+                --  2. example-2.com (we may have removed some headers here from 1)
+                --  3. example-1.com (since we are back at same host as 1, we need re-add stripped headers)
+                --
+                let strippedHeaders = filter (shouldStripHeaderOnRedirect r . fst) (requestHeaders origReq)
+                r{requestHeaders = mergeHeaders (requestHeaders r) strippedHeaders}
+
     stripHeaders' :: Request -> Request
     stripHeaders' r = r{requestHeaders =
                         filter (not . shouldStripHeaderOnRedirect req . fst) $
--- http-client/Network/HTTP/Client/Types.hs
+++ http-client/Network/HTTP/Client/Types.hs
@@ -620,8 +620,8 @@ data Request = Request
     -- ^ Decide whether a header must be stripped from the request
     -- when following a redirect, if host differs from previous request
     -- in redirect chain. Default: false (always strip regardless of host change)
-    -- 
-    -- @since 0.6.2
+    --
+    -- @since 0.7.15
 
     , proxySecureMode :: ProxySecureMode
     -- ^ How to proxy an HTTPS request.
